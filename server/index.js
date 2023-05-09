c66baee1f0b07c146f6bc0aeda4f5856158b3be3
Fernando Cases <nandocasesgarcia@gmail.com>
2023-05-10 01:00:21 +0200

    Add: basic api structure start

--- server/index.js
+++ server/index.js
@@ -4,6 +4,25 @@ const port = 3000;
 
 // routes
 
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      id: 1,
+      name: "Tyler",
+    },
+    {
+      id: 2,
+      name: "Fernando",
+    },
+    {
+      id: 3,
+      name: "Tiger",
+    },
+  ];
+
+  res.json(users);
+});
+
 //start
 
 app.listen(port, () => {
